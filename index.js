4e3577419546d90c5c39736b84737f343d3eeb98
overbye <overbye87@gmail.com>
2022-02-09 22:21:20 +0300

    add routes

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require("express");
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.end("Hello from express!");
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
