f621b6ad5f7670e172c87b46e17539bdf9329084
overbye <overbye87@gmail.com>
2022-02-14 14:47:15 +0300

    removed unnecessary

--- db/index.js
+++
@@ -1,9 +0,0 @@
-const Sequilize = require("sequelize");
-const sequelize = new Sequilize("postgres://postgres:root@localhost:5432/node");
-
-const User = require("./User.js")(sequelize);
-
-module.exports = {
-  sequelize: sequelize,
-  user: User,
-};
